aeb923c8814ab10d6595da042581ef8a7f3c5c70
Carlos Perez <carlosperez@MacBook-Pro-de-Carlos.local>
2018-04-19 19:54:24 -0600

    eliminacion
    eliminacion de archivos innecesarios

--- application/controllers/Sento.php
+++ application/controllers/Sento.php
@@ -1,16 +1,26 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Sento extends CI_Controller {
-  public function __construct()
-  	{
-  		parent::__construct();
-  	}
 
-    public function index()
+class Sento extends CI_Controller {
+
+	public function __construct()
 	{
-    $data = array(
-            'assets_uri' => base_url() . 'assets/'
-            );
-    $this->load->view('index/index', $data);
-  }
+		parent::__construct();
+	}
+
+  public function index()
+{
+  $data = array(
+          'assets_uri' => base_url() . 'assets/',
+					'libs_url' => base_url().'App/libs/',
+					'_url' => base_url().'App/'
+          );
+  $this->load->view('Sento/Sento', $data);
+}
+
+
+
 
 }
+
+/* End of file AjaxAdministracion.php */
+/* Location: ./application/controllers/AjaxAdministracion.php */
